aa3340a35cc5553adb354c1c449e6b8cdfa98d85
Greg Farrow <greg.farrow1@gmail.com>
2020-03-19 11:35:36 +0000

    updated tests

--- tests/integration_tests/cloudwatch/test_cloudwatch.py
+++ tests/integration_tests/cloudwatch/test_cloudwatch.py
@@ -7,22 +7,22 @@ from boto3_batch_utils import CloudwatchBatchDispatcher, cloudwatch_dimension
 
 
 @patch('boto3_batch_utils.Base.boto3', Mock())
-class TestSqsStandard(TestCase):
+class TestCloudwatch(TestCase):
 
     def test_more_than_one_batch_small_messages(self):
-        dy_client = CloudwatchBatchDispatcher(namespace='namey_name', max_batch_size=5)
+        cw_client = CloudwatchBatchDispatcher(namespace='namey_name', max_batch_size=5)
 
         mock_boto3 = Mock()
-        dy_client._aws_service = mock_boto3
-        dy_client._batch_dispatch_method = Mock()
+        cw_client._aws_service = mock_boto3
+        cw_client._batch_dispatch_method = Mock()
 
         for i in range(0, 6):
-            dy_client.submit_payload(metric_name='metricky', value=i+1, timestamp=datetime(2020, 2, 2, 1, 1, 1))
+            cw_client.submit_metric(metric_name='metricky', value=i+1, timestamp=datetime(2020, 2, 2, 1, 1, 1))
 
-        dy_client.flush_payloads()
+        cw_client.flush_payloads()
 
-        self.assertEqual(2, dy_client._batch_dispatch_method.call_count)
-        dy_client._batch_dispatch_method.assert_has_calls([
+        self.assertEqual(2, cw_client._batch_dispatch_method.call_count)
+        cw_client._batch_dispatch_method.assert_has_calls([
             call(**{'MetricData': [
                 {'MetricName': 'metricky', 'Timestamp': datetime(2020, 2, 2, 1, 1, 1), 'Value': 1, 'Unit': 'Count'},
                 {'MetricName': 'metricky', 'Timestamp': datetime(2020, 2, 2, 1, 1, 1), 'Value': 2, 'Unit': 'Count'},
